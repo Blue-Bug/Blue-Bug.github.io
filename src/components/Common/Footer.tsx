7384be3e2da773dd8cb58e50a8a363952b6dab18
Blue-Bug <kjj9987@gmail.com>
2023-06-11 21:47:55 +0900

    Initial Commit

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const FooterWrapper = styled.footer`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+
+  @media (max-width: 768px) {
+    font-size: 13px;
+  }
+`
+
+const Footer: FunctionComponent = function () {
+  return (
+    <FooterWrapper>
+      볼 건 없지만 많이 보고 가세요
+      <br />© 2023 Developer BlueBug, Powered By Gatsby.
+    </FooterWrapper>
+  )
+}
+
+export default Footer
---
+++ src/templates/post_template.tsx
@@ -0,0 +1,87 @@
+import Template from 'components/Common/Template'
+import CommentWidget from 'components/Post/CommentWidget'
+import PostContent from 'components/Post/PostContent'
+import PostHead from 'components/Post/PostHead'
+import { graphql } from 'gatsby'
+import React, { FunctionComponent } from 'react'
+import { PostFrontmatterType } from 'types/PostItem.types'
+
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[] // 존재하지 않는 타입이므로 에러가 발생하지만 일단 작성해주세요
+    }
+  }
+  location: {
+    href: string
+  }
+}
+
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+  location: { href },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+          publicURL,
+        },
+      },
+    },
+  } = edges[0]
+
+  return(
+    <Template title={title} description={summary} url={href} image={publicURL}>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html}/>
+      <CommentWidget/>
+    </Template>
+  ) 
+}
+
+export default PostTemplate
+
+export type PostPageItemType = {
+  node: {
+    html: string
+    frontmatter: PostFrontmatterType
+  }
+}
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
